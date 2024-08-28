7628a6f2afda817773f1499a366dbebf77b2ad8b
Trainer <admin@example.com>
2024-08-28 05:56:23 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LoginScreen from './screens/login/login.component'
+
+function App() {
+  return (
+    <>
+      <LoginScreen></LoginScreen>
+    </>
+  )
+}
+
+export default App
