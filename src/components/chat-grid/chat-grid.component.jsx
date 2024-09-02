8d572c65f02363c3a7017ed68ee07e9eeb5aa59f
Trainer <admin@example.com>
2024-09-02 11:27:24 +0100

    Added classnames npm

--- src/components/chat-grid/chat-grid.component.jsx
+++ src/components/chat-grid/chat-grid.component.jsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
 import { ThemeOptions, useTheme } from "../../hooks/useTheme";
+import classNames from "classnames";
 
 const ChatGrid = () => {
   const { theme } = useTheme();
+
+  const containerClasses = classNames(
+    "p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4",
+    {
+      "bg-white": theme === ThemeOptions.LIGHT,
+      "bg-slate-400": theme === ThemeOptions.DARK,
+    }
+  );
+
+  const gridItemClass = classNames(" p-6 rounded-lg shadow-md", {
+    "bg-white": theme === ThemeOptions.LIGHT,
+    "bg-slate-600": theme === ThemeOptions.DARK,
+  });
 
   const chats = [
     {
@@ -19,14 +33,10 @@ const ChatGrid = () => {
     },
   ];
   return (
-    <div
-      className={`p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ${
-        theme === ThemeOptions.LIGHT ? "bg-white" : "bg-slate-400"
-      }`}
-    >
+    <div className={containerClasses}>
       {chats.map((chat) => {
         return (
-          <div key={chat.id} className="bg-white p-6 rounded-lg shadow-md">
+          <div key={chat.id} className={gridItemClass}>
             <p className="text-center text-gray-800">
               <Link to={`/chat/${chat.id}`}>{chat.heading}</Link>
             </p>
